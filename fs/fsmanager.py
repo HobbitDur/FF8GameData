5c4331cb928cabff0b1dfc00cee4f495ca15eb7c
LudovicGuerra <ludovic791@gmail.com>
2024-10-18 01:54:58 +0200

    Working on lzs

--- fs/fsmanager.py
+++ fs/fsmanager.py
@@ -1,9 +1,10 @@
 import glob
 import os
 import pathlib
+import time
 from dataclasses import dataclass
 from enum import Enum, auto
-import lzss
+from typing import Generator
 
 from fs.lzs import Lzs
 
@@ -17,11 +18,12 @@ class FsFileType(Enum):
     BATTLE = auto()
     UNKNOWN = auto()
 
+
 @dataclass
 class FiSingleData:
-        length_unpack_file: int
-        packed_file_location: int
-        compression_used: bool
+    length_unpack_file: int
+    packed_file_location: int
+    compression_used: bool
 
 
 class Archive:
@@ -49,6 +51,7 @@ class Archive:
         self._fs_data = bytearray()
         self._fi_data = bytearray()
         self._fl_data = []
+        self.lzs = Lzs()
 
         # Data analysed now
         self.name = pathlib.Path(fs_path).name.replace(".fs", "")
@@ -58,19 +61,21 @@ class Archive:
         else:
             self.type = self.FILE_NAME_LIST[self.FILE_NAME_STR_LIST.index(self.name)]
 
+
         # Data analysed later
         self._file_name_list = []
         self._data_list = []
         self._fi_data_list = []
         self._nb_file = 0
+        self._fs_file_size = 0
 
     def __str__(self):
         loaded = False
         analysed = False
         if self._fl_data and self._fi_data and self._fs_data:
-            loaded= True
-        if self._file_name_list and self._nb_file > 0 and  self._fi_data_list and self._data_list:
-            analysed= True
+            loaded = True
+        if self._file_name_list and self._nb_file > 0 and self._fi_data_list and self._data_list:
+            analysed = True
         return f"Archive(name:{self.name}, loaded:{loaded}, analysed:{analysed})"
 
     def __repr__(self):
@@ -106,40 +111,47 @@ class Archive:
         """
         # Checking if data have been loaded previously
         if not self._fs_data or not self._fi_data or not self._fl_data:
+            print("Wasn't loaded")
             self.load_data()
-
         # For FL, the data is already a list of text
         self._file_name_list = self._fl_data
         self._nb_file = len(self._file_name_list)
-
         # FI analyse
-        for current_offset in range(0, len(self._fi_data), 3*self.OFFSET_SIZE):
-            length_unpack_file = int.from_bytes(self._fi_data[current_offset:current_offset +self.OFFSET_SIZE], byteorder="little")
-            packed_file_location = int.from_bytes(self._fi_data[current_offset+self.OFFSET_SIZE:current_offset +self.OFFSET_SIZE*2], byteorder="little")
-            compression_used = bool(int.from_bytes(self._fi_data[current_offset+self.OFFSET_SIZE*2:current_offset +self.OFFSET_SIZE*3], byteorder="little"))
+        for current_offset in range(0, len(self._fi_data), 3 * self.OFFSET_SIZE):
+            length_unpack_file = int.from_bytes(self._fi_data[current_offset:current_offset + self.OFFSET_SIZE], byteorder="little")
+            packed_file_location = int.from_bytes(self._fi_data[current_offset + self.OFFSET_SIZE:current_offset + self.OFFSET_SIZE * 2], byteorder="little")
+            compression_used = bool(
+                int.from_bytes(self._fi_data[current_offset + self.OFFSET_SIZE * 2:current_offset + self.OFFSET_SIZE * 3], byteorder="little"))
             self._fi_data_list.append(FiSingleData(length_unpack_file, packed_file_location, compression_used))
-
         # FS analyse
-        for i in range(self._nb_file):
+        self._fs_file_size = int.from_bytes(self._fs_data[0:4], byteorder='little')
+        for i in range(0, self._nb_file):
             if i == self._nb_file - 1:
                 end_data = len(self._fs_data)
             else:
-                end_data = self._fi_data_list[i+1].packed_file_location
-            start_data = self._fi_data_list[i].packed_file_location
-            fs_data = self._fs_data[start_data:end_data]
+                end_data = self._fi_data_list[i + 1].packed_file_location
+            start_data = self._fi_data_list[i].packed_file_location + 4 # 4 bytes for length of file at start
             if self._fi_data_list[i].compression_used:
-                #fs_data = lzss.decompress(bytes(fs_data))
-                fs_data = Lzs.decode(fs_data)
-                #fs_data = lzs_c.decode(fs_data)
-                pass
-            self._data_list.append(fs_data)
+                self._data_list.append(self.lzs.decode(self._fs_data[start_data:end_data]))
+            else:
+                self._data_list.append(self._fs_data[start_data:end_data])
 
-    def get_fs_data_analyzed(self)->list[bytearray]:
-        """Get the FS data uncompressed"""
+    def get_generator_fs_data_analyzed(self) -> list[Generator[int, None, None]]:
         return self._data_list
-    def get_fi_data_analyzed(self)->list[FiSingleData]:
+
+    def get_fs_data_analyzed(self) -> list[bytes]:
+        print("get_fs_data_analyzed_computed")
+        data_byte = []
+        for data in self._data_list:
+            data_byte.append(bytes(data))
+            print(f"len data: {len(data_byte[-1])}")
+        print(f"len: {len(data_byte)}")
+        return data_byte
+
+    def get_fi_data_analyzed(self) -> list[FiSingleData]:
         return self._fi_data_list
-    def get_fl_data_analyzed(self)->list[str]:
+
+    def get_fl_data_analyzed(self) -> list[str]:
         return self._file_name_list
 
 
@@ -149,6 +161,7 @@ class FsManager:
     The aim is to provide an easy-to-use abstract of a list of archive
     It is totally possible to manager his own list as the Archive class works on its own.
     """
+
     def __init__(self, folder_path=None):
         self._archive_list = []
         if folder_path:
@@ -163,11 +176,11 @@ class FsManager:
     def __repr__(self):
         return self.__str__()
 
-    def preload_all_archive_by_default_name(self, folder_path:str):
+    def preload_all_archive_by_default_name(self, folder_path: str):
         for file_name in Archive.FILE_NAME_STR_LIST:
             self._archive_list.append(Archive.from_folder_and_name(folder_path, file_name))
 
-    def preload_all_archive_in_folder(self, folder_path:str):
+    def preload_all_archive_in_folder(self, folder_path: str):
         fs_file_in_folder = glob.glob(os.path.join(folder_path, "*.fs"))
         for fs_file in fs_file_in_folder:
             if os.path.exists(fs_file.replace(".fs", ".fi")):
@@ -193,30 +206,31 @@ class FsManager:
     def analyse_archive_by_name(self, name):
         self.get_archive_by_name(name).analyse_data()
 
-    def get_data_by_name(self, name:str):
+    def get_data_by_name(self, name: str):
         self.get_archive_by_name(name).get_fs_data_analyzed()
 
-    def get_archive_by_name(self, name:str) -> Archive:
+    def get_archive_by_name(self, name: str) -> Archive:
         for archive in self._archive_list:
             if archive.name == name:
                 return archive
+        print(f"Name not found in the archive: {name}")
 
 
 if __name__ == "__main__":
     # Create the fsManager
     fs_manager = FsManager()
-
     # Now do the full workflow:
     # 1. Preload (means reading the folder to find the different files)
     fs_manager.preload_all_archive_in_folder(os.path.join("..", "FsTester"))
     # 2. Load (means reading the data, which take memory and time)
-    fs_manager.load_all_archive()
+    #fs_manager.load_all_archive()
+    fs_manager.load_archive_by_name("magic")
     # 3. Analyse (means analysing the data previously loaded, take time)
     fs_manager.analyse_archive_by_name("magic")
 
     # Retrieving as an example the archive for magic
     archive_magic = fs_manager.get_archive_by_name("magic")
-    archive_fs_data = archive_magic.get_fs_data_analyzed()
+    archive_fs_data = archive_magic.get_generator_fs_data_analyzed()
     archive_fi_data = archive_magic.get_fi_data_analyzed()
     archive_fl_data = archive_magic.get_fl_data_analyzed()
 
@@ -225,9 +239,11 @@ if __name__ == "__main__":
     print(f"Fi archive data: {archive_fi_data[0]}")
     print(f"Fi archive data: {archive_fi_data[1]}")
     print(f"Fi archive data: {archive_fi_data[2]}")
-    print(f"Fs hex: {archive_fs_data[0].hex(" ")}")
-    print(f"Len hex: {len(archive_fs_data[0])}")
-    print(f"Len hex: {len(archive_fs_data[1])}")
+    # Fast method to read hex (as nothing is loaded till needed)
+    #print(f"Fs hex: {bytes(archive_fs_data[0]).hex(" ")}")
+    # Slow method to read hex (as everything is loaded in memory ready to be used)
+    #archive_fs_data_bytes = archive_magic.get_fs_data_analyzed()
+    #print(f"Fs hex2: {archive_fs_data_bytes[0].hex(" ")}")
 
     # Print the fs manager, which will show all the archive loaded
-    #print(fs_manager)
+    # print(fs_manager)
