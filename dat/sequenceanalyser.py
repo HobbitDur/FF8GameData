36aa007325aeb7e4b470a7e166c782531d46664a
HobbitDur <ludovic791@gmail.com>
2025-07-03 17:41:11 +0200

    Working on sequence analyser

--- dat/sequenceanalyser.py
+++ dat/sequenceanalyser.py
@@ -3,7 +3,7 @@ from ..gamedata import GameData
 
 class SequenceAnalyser:
 
-    def __init__(self, game_data: GameData, model_anim_data,sequence: bytearray):
+    def __init__(self, game_data: GameData, model_anim_data, sequence: bytearray):
         self._sequence = sequence
         self._model_anim_data = model_anim_data
         self.game_data = game_data
@@ -23,63 +23,74 @@ class SequenceAnalyser:
         all_op_info = self.game_data.anim_sequence_data_json['op_code_info']
         op_code = []
         index_data = 0
+        text_analyze = ""
         while index_data < len(self._sequence):
             hex_data = self._sequence[index_data]
+            current_opcode_int = int.from_bytes(hex_data)
+            current_opcode_hex_str = "0x" + (hex_data.hex()).upper()
+            text_analyze += f"{(hex_data.hex(" ")).upper()} "
+            current_op_code_data = [x for x in self.game_data.anim_sequence_data_json["op_code_info"] if int(x['opcode'], 16) == current_opcode_int]
+            if current_op_code_data:
+                current_op_code_data = current_op_code_data[0]
+            else:
+                print(f"Opcode {current_opcode_int} not found, assuming 0 param")
+                text_analyze += ": Unknown"
+                continue
 
-            if hex_data < self._model_anim_data['nb_animation']:
-                print("Op id {:02X}  means play anim {}".format(hex_data, hex_data))
-                index_data+=1
+            # Reading param
+            if current_op_code_data["size"] == -1:
+                current_op_code_size = 1
+            else:
+                current_op_code_size = current_op_code_data["size"]
+            nb_param_analyzed = 0
+            param_list = []
+            while True:
+                if nb_param_analyzed == current_op_code_size:# If we analyzed all param
+                    break
+                index_data += 1
+                current_param = self._sequence[index_data]
+                param_list.append(current_param)
+                current_param_int = int.from_bytes(current_param)
+                text_analyze +=  f"{(current_param.hex(" ")).upper()} "
+                nb_param_analyzed += 1
+                if current_opcode_int in [0x99, 0xB1]: #Last one is FF
+                    if current_param_int != 0xFF:
+                        current_op_code_size +=1
+                    else:
+                        break
+                if current_opcode_int == 0xB0:
+                    print("Todo !")
+                    break
+                if current_opcode_int == 0xB8:
+                    print("Todo !")
+                    break
+            text_analyze += ": "
+
+            # Now analyzing the opcode
+            if current_opcode_int < 0x80:
+                text_analyze += f"Play anim {current_opcode_hex_str}"
             else:
                 # Searching the data in op code list
-
-                op_info = [x for x in all_op_info if x['op_code'] == hex_data]
-                if op_info:
-                    op_info = op_info[0]
-                else:
-                    print("op code {:02X} not found".format(hex_data))
-                    index_data += 1
-                    continue
-                    op_info = {"op_code": 0x00, "size": 0, "text": "Unknown", "param_index": [], "complexity": "simple", "param_type": [], "param_list": []}
-                if op_info['complexity'] == "simple":
-                    op_code = self._sequence[index_data+1: index_data + op_info['size']+1]
-                    op_code_text_param = []
-                    for index_param, param_type in enumerate(op_info['param_type']):
+                if current_op_code_data['complexity'] == "simple":
+                    for index_param_in_str, param_type in enumerate(current_op_code_data['param_type']):
+                        param_index = current_op_code_data['param_index'][index_param_in_str]
+                        param_data = param_list[param_index]
                         # For the moment we mainly show int, but in the futur we will show text linked to this ID
-                        param_index = op_info['param_index'][index_param]
-                        param_data = op_code[param_index]
                         if param_type == "anim_id":
-                            op_code_text_param.append(int(param_data))
-                        if param_type == "seq_id":
-                            op_code_text_param.append(int(param_data))
-                        if param_type == "effect_id":
-                            op_code_text_param.append(
-                                int.from_bytes(bytes([op_code[param_index], op_code[param_index+1]]), byteorder="little"))
-                        if param_type == "start_anim_id":
-                            op_code_text_param.append(
-                                int.from_bytes(bytes([op_code[param_index], op_code[param_index+1]]), byteorder="little"))
-                        if param_type == "local_sound_id":
-                            op_code_text_param.append(
-                                int.from_bytes(bytes([op_code[param_index], op_code[param_index+1]]), byteorder="little"))
-                        if param_type == "general_sound_id":
-                            op_code_text_param.append(
-                                int.from_bytes(bytes([op_code[param_index], op_code[param_index+1]]), byteorder="little"))
-                        if param_type == "aura_id":
-                            print("arua")
-                            op_code_text_param.append(
-                                int.from_bytes(bytes([op_code[param_index], op_code[param_index+1]]), byteorder="little"))
-                        if param_type == "visibility":
-                            if param_data == 0x02:
-                                op_code_text_param.append("Hide")
-                            elif param_data == 0x03:
-                                op_code_text_param.append("Show")
-                            else:
-                                op_code_text_param.append("Unknown_visibility")
-                    op_code_text = op_info['text']
-                    if op_code_text_param:
-                        op_code_text = op_code_text.format(*op_code_text_param)
-                    print("Op id {:02X} with Op code {} means {}".format(op_info['op_code'], ' '.join("{:02X}".format(byte) for byte in op_code), op_code_text))
-                    index_data += op_info['size']+1
-                elif op_info['complexity'] == "complex":
+                            text_analyze += f"Play anim {int.from_bytes(param_data, byteorder="little", signed=False )}"
+                        elif param_type == "effect_id":
+                            text_analyze += f"{int.from_bytes(param_data, byteorder="little", signed=False )}"
+                        elif param_type == "fade_effect_id":
+                            text_analyze +=  f"{int.from_bytes(param_data, byteorder="little", signed=False )}"
+                        elif param_type == "ubyte":
+                            text_analyze +=  f"{int.from_bytes(param_data, byteorder="little", signed=False )}"
+                        elif param_type == "sbyte":
+                            text_analyze +=  f"{int.from_bytes(param_data, byteorder="little", signed=True )}"
+                        elif param_type == "int16":
+                            text_analyze +=  f"{int.from_bytes(param_list[param_index: param_index+1], byteorder="little", signed=True )}"
+                        else:
+                            text_analyze += "Unknown type parameter"
+                elif current_op_code_data['complexity'] == "complex":
                     added_text = ""
                     if op_info['op_code'] == 0xc1:
                         if self._sequence[index_data + 1] == 0x00 and self._sequence[index_data + 2] == 0xe5 and self._sequence[index_data + 3] == 0x0f:
@@ -118,8 +129,7 @@ class SequenceAnalyser:
                     else:
                         op_code_text = "None"
                     print("Op code {:02X} with Op code {} means {}".format(op_info['op_code'], op_code_text, added_text))
-
-
+            text_analyze += "\n"
         print("End __analyse_sequence")
 
     def get_size(self):
